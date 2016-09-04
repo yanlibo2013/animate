49845da8a7eccedb917b2b8f90626f797d1c7816
yanlibo <yaniibo2010@163.com>
2016-09-04 22:46:37 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
+var minify = require('gulp-minifier');
 var gulp = require('gulp');
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
-
-gulp.task('default', function () {
-    gulp.src('style/**/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/style'));
+gulp.task('cssmin', function() {
+    return gulp.src(['./style/index.css','./style/run.css']).pipe(minify({
+        minify: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+        minifyCSS: true,
+        getKeptComment: function (content, filePath) {
+            var m = content.match(/\/\*![\s\S]*?\*\//img);
+            return m && m.join('\n') + '\n' || '';
+        }
+    })).pipe(gulp.dest('./dist/style'));
 });
+
+gulp.task('minifyJS', function() {
+    return gulp.src(['./javascript/jquery.mousewheel.js','./javascript/slidepages.js']).pipe(minify({
+        minify: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+        minifyCSS: true,
+        getKeptComment: function (content, filePath) {
+            var m = content.match(/\/\*![\s\S]*?\*\//img);
+            return m && m.join('\n') + '\n' || '';
+        }
+    })).pipe(gulp.dest('./dist/javascript'));
+});
+
+
+gulp.task('minifyHtml', function() {
+    return gulp.src(['./index.html']).pipe(minify({
+        minify: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+        minifyCSS: true,
+        getKeptComment: function (content, filePath) {
+            var m = content.match(/\/\*![\s\S]*?\*\//img);
+            return m && m.join('\n') + '\n' || '';
+        }
+    })).pipe(gulp.dest('./dist/'));
+});
+
+gulp.task("default",["cssmin","minifyJS"]);
